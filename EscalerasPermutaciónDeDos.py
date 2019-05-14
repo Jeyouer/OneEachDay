60534c526d07bfe083462a74d57adcb6d509003a
JRivas <jotarivper@gmail.com>
2019-05-14 13:01:32 +0200

    Starting OneEachDay problem

---
+++ EscalerasPermutaciónDeDos.py
@@ -0,0 +1,8 @@
+# This program calculates the whole variety of forms to raise a stair
+# which has k steps. In each step you can decide raise one or two steps.
+
+
+k=5
+SolutionVector = {0,0,0,0}
+def AddPossibleSolution():
+	SolutionVector >> 1
